7088c382404b05684f65a56e4c1b91e3d1659e6e
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-22 16:47:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
   word.match(/[aeiou]/)
     return true
-  word.match(/[!aeiou]/)
+  word.match(/![aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
